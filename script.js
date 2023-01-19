a51564ea402d1ab5cc1403f49228a092644c5f21
fatuousnerd <calvinkmwaura@gmail.com>
2023-01-19 10:58:52 +0300

    Update

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-var load = document.querySelector(".load");
+var load = document.querySelector(".load"),
 loader = document.getElementById("loader"),
-card = document.querySelectorAll(".card");
+artists = document.getElementById("artists"),
+artistlist = document.getElementById("artist-list"),
+player = document.getElementById("player"),
+art = document.getElementById("art"),
+tr = document.getElementById("tr"),
+img = document.getElementById("img"),
+card = document.querySelectorAll(".card"),
+icn = document.querySelectorAll(".icn"),
+currentIndex = 0;
+pname = artistlist.getElementsByTagName("h2");
 
 function search(){
     const searchbox = document.getElementById("search-item").value.toUpperCase();
-    const storeitem = document.getElementById("search-list");
+    const storeitem = document.getElementById("artist-list");
     const card = document.querySelectorAll(".card");
     const pname = storeitem.getElementsByTagName("h2");
 
@@ -27,6 +36,80 @@ window.addEventListener('load', () => {
     //loader.style.display = 'none'
 });
 
-card.mouseover = () => {
-    console.warn("Hovered!!")
+
+let recentSearches = [];
+    
+function addSearch(query) {
+  recentSearches.unshift(query);
+  updateList();
+}
+
+function updateList() {
+  let list = document.getElementById("recent-searches");
+  list.innerHTML = "";
+  for (let i = 0; i < recentSearches.length; i++) {
+    let item = document.createElement("li");
+    item.textContent = recentSearches[i];
+    list.appendChild(item);
+  }
+}
+
+artists.onclick = () => {
+  artistlist.classList.toggle("none")
+}
+
+/*function sing(){
+  let currentIndex = 0
+  player.src = durkioo[currentIndex]
+}
+
+sing()*/
+
+card.forEach(function(card) {
+  card.onmouseover = () => {
+     // icn.classList.remove("none")
+  }
+  card.onclick = () => {
+    let hi = [pname]
+    console.log(hi)
+    for(i = 0; i < pname.length; i++){
+      console.log(pname[i])
+    }
+    img.src = durkioo[currentIndex].img;
+    art.textContent = durkioo[currentIndex].ar
+    tr.textContent = durkioo[currentIndex].track
+    player.src = durkioo[currentIndex].song
+    //player.load()
+  }
+});
+
+function play() {
+  if(player.paused == true){
+    player.play()
+  } else{
+    player.pause()
+  }
+  //player.play();
+  //player.src = durkioo[currentIndex]
+}
+
+function pause() {
+  player.pause();
+}
+
+function prev() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = durkioo.length - 1;
+  }
+  player.src = durkioo[currentIndex];
+  play();
+}
+
+function next() {
+  currentIndex++;
+  if (currentIndex >= durkioo.length) {
+    currentIndex = 0;
+  }
+  player.src = durkioo[currentIndex];
 }
---
+++ songs/songs.js
@@ -0,0 +1,25 @@
+const durkioo = [
+    {
+        img: 'Images/Artist/lildurk.jfif',
+        ar: 'Lil Durk, Only The Family',
+        track: 'Hellcats And Trackhawks',
+        song: 'songs/Only The Family  Lil Durk  Hellcats  Trackhawks.mp3'
+        
+    },
+    {
+        img: 'Images/tunify.png',
+        ar: 'Lil Durk, Pooh Shiesty',
+        track: 'Back In Blood',
+        song: 'songs/Pooh Shiesty  Back In Blood feat Lil Durk Official Audio.mp3'
+        
+    },
+    {
+        img: '',
+        ar: 'Lil Durk, Gucci Mane',
+        track: 'Rumors',
+        song: 'songs/Gucci Mane  Rumors feat Lil Durk Official Video.mp3'
+        
+    }
+]
+
+console.log("HTML")
